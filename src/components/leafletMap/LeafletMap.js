b3a14bc875393ac6957ba71491728fd43f4b8363
jovid1242 <jovid1242@gmail.com>
2022-03-19 19:56:30 +0500

    add marker for map

--- src/components/leafletMap/LeafletMap.js
+++ src/components/leafletMap/LeafletMap.js
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
-import { Map, TileLayer } from "react-leaflet";
+import React from "react";
+import "../../styles/map.scss";
+import L from "leaflet";
+import { Marker, MapContainer, Polyline, Popup } from "react-leaflet";
+
+const center = [55.756179, 37.61588];
+
+const polyline = [
+  [55.756179, 37.61588],
+  [55.731357, 37.576579],
+];
+
+const limeOptions = { color: "red" };
 
 export default function LeafletMap() {
-  const [position, setPosition] = useState({
-    lat: 55.702868,
-    lng: 37.530865,
-    zoom: 10,
-  });
+  // указываем путь к файлам marker
+  L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 
-  var center = [position.lat, position.lng];
   return (
     <div className="map_wrapper">
-      <Map
-        zoom={position.zoom}
-        center={center}
-        dragging={false}
-        zoomControl={false}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      </Map>
+      <MapContainer center={center} zoom={13}>
+        <Marker position={[55.756179, 37.61588]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={[55.731357, 37.576579]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Polyline pathOptions={limeOptions} positions={polyline} />
+      </MapContainer>
     </div>
   );
 }
